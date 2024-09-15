9aaf9e7c63779e10d5fb33e26c1a3b265b7ca2fa
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-09-15 14:40:56 +0400

    Начал заполнять доменные элементы

---
+++ Domain/Entitys/Product.cs
@@ -0,0 +1,15 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitys
+{
+    public class Product : Entity<Guid>
+    {
+        public decimal DefaultPrice { get; private set; }
+        public IReadOnlyList<Trait> Traits { get; private set; }
+    }
+}
